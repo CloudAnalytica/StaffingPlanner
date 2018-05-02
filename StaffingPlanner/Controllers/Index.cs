7792ec8d52cf8d75a50f4c5c591405b8a3760239
mdng223 <mdng223@g.uky.edu>
2018-05-02 13:53:51 -0400

    Deleted the views: About.cshtml, Contact.cshtml. Deleted the controller functions: associated with the views.

---
+++ StaffingPlanner/Controllers/Index.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StaffingPlanner.Models;
+
+namespace StaffingPlanner.Controllers
+{
+    public class Index : Controller
+    {
+        private ClientOpportunitiesEntities db = new ClientOpportunitiesEntities();
+
+        // GET: Projects
+        public ActionResult IndexGet()
+        {
+            var projects = db.Projects.Include(p => p.Client).Include(p => p.ProjectStatus).Include(p => p.ProjectRole);
+            return View(projects.ToList());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
