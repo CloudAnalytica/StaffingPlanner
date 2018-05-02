c2a01b549d6657073781d936a0a8c7392b278d1b
mdng223 <mdng223@g.uky.edu>
2018-05-02 14:33:43 -0400

    Messing around with tables for Report.cshtml

--- StaffingPlanner/Controllers/HomeController.cs
+++ StaffingPlanner/Controllers/HomeController.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using StaffingPlanner.Models;
+
 
 namespace StaffingPlanner.Controllers
 {
     public class HomeController : Controller
     {
+        private ClientOpportunitiesEntities db = new ClientOpportunitiesEntities();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET: Projects
         public ActionResult Report()
         {
-            return View();
+            var projects = db.Projects.Include(p => p.Client).Include(p => p.ProjectStatus).Include(p => p.ProjectRole);
+            return View(projects.ToList());
         }
     }
 }
