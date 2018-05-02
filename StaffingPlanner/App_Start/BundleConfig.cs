dc397d804c8c65c18109a5943ddfc8afdc0479a2
mdng223 <mdng223@g.uky.edu>
2018-05-02 15:28:55 -0400

    Deleted fontawesome package. Added fontawesome cdn. Added icon for report.cshtml

--- StaffingPlanner/App_Start/BundleConfig.cs
+++ StaffingPlanner/App_Start/BundleConfig.cs
@@ -20,12 +20,13 @@ namespace StaffingPlanner
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
+
     }
 }
