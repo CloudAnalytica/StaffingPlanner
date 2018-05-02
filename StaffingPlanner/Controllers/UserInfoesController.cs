2e57859173edfb323ca7d7e9fe72cd4153a66ea0
mdng223 <mdng223@g.uky.edu>
2018-05-02 18:24:45 -0400

    Create UserInfos view. The Create is going to be used for register. Created Log in. Still waiting on password field. Created Userinfos controller

---
+++ StaffingPlanner/Controllers/UserInfoesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StaffingPlanner.Models;
+
+namespace StaffingPlanner.Views
+{
+    public class UserInfoesController : Controller
+    {
+        private ClientOpportunitiesEntities db = new ClientOpportunitiesEntities();
+
+        // GET: UserInfoes
+        public ActionResult Index()
+        {
+            var userInfoes = db.UserInfoes.Include(u => u.UserPrivilege);
+            return View(userInfoes.ToList());
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: UserInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserPrivilegeId = new SelectList(db.UserPrivileges, "UserPrivilegeId", "UserPrivilegeName");
+            return View();
+        }
+
+        // POST: UserInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userId,UserPrivilegeId,username,email,firstName,lastName")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                userInfo.UserPrivilegeId = 6;
+                db.UserInfoes.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserPrivilegeId = new SelectList(db.UserPrivileges, "UserPrivilegeId", "UserPrivilegeName", userInfo.UserPrivilegeId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserPrivilegeId = new SelectList(db.UserPrivileges, "UserPrivilegeId", "UserPrivilegeName", userInfo.UserPrivilegeId);
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "userId,UserPrivilegeId,username,email,firstName,lastName")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserPrivilegeId = new SelectList(db.UserPrivileges, "UserPrivilegeId", "UserPrivilegeName", userInfo.UserPrivilegeId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
